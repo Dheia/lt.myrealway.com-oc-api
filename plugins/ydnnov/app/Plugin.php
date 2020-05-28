8d4a58d7cb3de237ccb625daa88e9e6294f1a292
Yuriy <juriy9@yandex.ru>
2020-05-28 12:08:15 +0000

    Added app plugin

---
+++ plugins/ydnnov/app/Plugin.php
@@ -0,0 +1,17 @@
+<?php namespace Ydnnov\App;
+
+use Cms\Classes\Controller;
+use October\Rain\Foundation\Console\Kernel;
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+
+}
