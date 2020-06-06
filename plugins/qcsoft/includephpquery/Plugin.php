4ff0c65e001c41c7c3ae46375cd4d209784d2be7
Yuriy <juriy9@yandex.ru>
2020-06-06 07:20:39 +0000

    Created plugin for importing phpQuery library

---
+++ plugins/qcsoft/includephpquery/Plugin.php
@@ -0,0 +1,16 @@
+<?php namespace Qcsoft\Includephpquery;
+
+use System\Classes\PluginBase;
+
+require('phpQuery-master/phpQuery/phpQuery.php');
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
